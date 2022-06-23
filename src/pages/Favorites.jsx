9a41f132325942e4e2d1b6226073ee6299f19b23
alexhetley6107 <alexbredun1996@gmail.com>
2022-06-23 18:12:48 +0300

    fix layuot and some bugs

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -6,35 +6,32 @@ import Info from '../components/Info';
 import { emptyFav } from '../img/Images';
 
 const Favorites = () => {
+	const { favorites, onAddToFavorites } = useContext(AppContext);
+	const navigate = useNavigate();
 
-  const {favorites, onAddToFavorites} = useContext(AppContext);
-  const navigate = useNavigate();
+	const returnToHome = () => {
+		navigate('/sneakers');
+	};
 
-  const returnToHome = () => {
-    navigate('/')
-  }
-
-  return (
-    <div className="content p-40">
-      <h1 className="mb-30" >Мои закладки</h1>
-      <div className="favorites d-flex">
-      { favorites.length >0 
-        ?
-        favorites.map((item , index)=>
-          <Card key={index}
-            {...item}  
-            onFavorite={onAddToFavorites}
-            favorited={true}/>)
-        :
-        <Info img={emptyFav} 
-          title='Закладок нет :(' 
-          desc='Вы ничего не добавляли в закладки'
-          method={returnToHome}  
-        />
-      }
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className='content p-40'>
+			<h1 className='mb-30'>Мои закладки</h1>
+			<div className='favorites d-flex'>
+				{favorites.length > 0 ? (
+					favorites.map((item, index) => (
+						<Card key={index} {...item} onFavorite={onAddToFavorites} favorited={true} />
+					))
+				) : (
+					<Info
+						img={emptyFav}
+						title='Закладок нет :('
+						desc='Вы ничего не добавляли в закладки'
+						method={returnToHome}
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
 
 export default Favorites;
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,57 +8,49 @@ import Info from '../components/Info';
 import { emptyOrders } from '../img/Images';
 
 const Orders = () => {
+	const [orders, setOrders] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-  const [orders, setOrders] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);  
+	const { onAddToCart, onAddToFavorites } = useContext(AppContext);
 
-  const {onAddToCart, onAddToFavorites} = useContext(AppContext)
+	const navigate = useNavigate();
 
-  const navigate = useNavigate();
+	const returnToHome = () => {
+		navigate('/sneakers');
+	};
 
-  const returnToHome = () => {
-    navigate('/')
-  }
+	useEffect(() => {
+		(async () => {
+			try {
+				const { data } = await axios.get('https://629b626bcf163ceb8d18cc73.mockapi.io/orders');
+				setOrders(data.reduce((prev, obj) => [...prev, ...obj.items], []));
+				setIsLoading(false);
+			} catch (error) {
+				alert('Oшибка при запросе заказов');
+				console.error(error);
+			}
+		})();
+	}, []);
 
-  useEffect(() => {    
-    (async () => {
-      try {
-        const {data} = await axios
-          .get('https://629b626bcf163ceb8d18cc73.mockapi.io/orders');
-        setOrders(data.reduce((prev, obj) => [...prev, ...obj.items], []))
-        setIsLoading(false)
-      } catch (error) {
-        alert('Oшибка при запросе заказов');
-        console.error(error);
-      }
-    })();
-    
-
-  }, [])
-
-  return (
-    <div className="content p-40">
-      <h1 className="mb-30" >Мои заказы</h1>
-      <div className="favorites d-flex">
-      {
-        isLoading ? [...Array(4)] : 
-          orders.length > 0 ?      
-        
-          orders.map((item , index)=>
-            <Card key={index}
-              {...item}  
-              loading={isLoading}
-              />)
-              :
-              <Info img={emptyOrders}
-                method={returnToHome}
-                title='У вас нет заказов'
-                desc='Вы нищеброд?  Оформите хотя бы один заказ.'
-              />
-      }
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className='content p-40'>
+			<h1 className='mb-30'>Мои заказы</h1>
+			<div className='favorites d-flex'>
+				{isLoading ? (
+					[...Array(4)]
+				) : orders.length > 0 ? (
+					orders.map((item, index) => <Card key={index} {...item} loading={isLoading} />)
+				) : (
+					<Info
+						img={emptyOrders}
+						method={returnToHome}
+						title='У вас нет заказов'
+						desc='Вы нищеброд?  Оформите хотя бы один заказ.'
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
 
 export default Orders;
